8e0dd1e2adfedddda848915dfe256cb316a13de8
rolandoparedes-sketch <rolando.paredes@tecsup.edu.pe>
2026-04-17 21:57:18 -0500

    Hitos

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public CustomDoubleLinkedList snapshotSystem = new();
+
+    public Player player;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+
+    }
+    [Button]
+    public void SaveTurn()
+    {
+        snapshotSystem.SaveTurn();
+        Debug.Log("Saving turn: " + snapshotSystem.Count);
+    }
+    //[Button]
+    public void LoadTurn()
+    {
+        snapshotSystem.LoadTurn(player);
+    }
+    [Button]
+    public void NextTurn()
+    {
+        snapshotSystem.MoveForward();
+        LoadTurn();
+    }
+    [Button]
+    public void PrevTurn()
+    {
+        snapshotSystem.MoveBackwards();
+        LoadTurn();
+    }
+
+
+}
